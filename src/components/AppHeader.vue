c1c33df20463ab72409095fd6b6e2608c9db7e3e
mikzanco <mikyzancolo97@gmail.com>
2022-11-17 15:22:32 +0100

    first commit Breaking bad, ADD header with logo and h1 title

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,11 @@ export default {
 </script>
 
 <template>
-  <div>Header</div>
+  <header>
+    <img src="/logo.svg" alt="">
+    <h1>Breaking Bad Api</h1>
+  </header>
+  
 </template>
 
 
@@ -13,4 +17,16 @@ export default {
 <style lang="scss" scoped>
 
   @use '../style/partials/variables' as *;
+  header{
+    display: flex;
+    img{
+    width: 100px;
+    height: 50px;
+    }
+    h1{
+      font-weight: 500 ;
+    }
+  }
+  
+
 </style>
